6e6d71b64a7b21edd4151ba13a1d17377ac17df1
jkweb-dev <jk.buildsweb@gmail.com>
2026-07-05 12:40:58 -0700

    Inital Project Setup

---
+++ Frontend/app/page.js
@@ -0,0 +1,14 @@
+ import { Button } from "@/Components/ui/button"
+ 
+ const Home = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <Button>
+        Click Me
+      </Button>
+    </div>
+  )
+}
+
+
+export default Home
